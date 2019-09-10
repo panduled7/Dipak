295a018e5946b400c3cda776e2a5c38e4e73fd44
Sagar Jadhav <ubuntu@ip-172-31-23-162.us-east-2.compute.internal>
2019-09-10 17:52:45 +0000

    Done

--- calc.py
+++ calc.py
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 def add(x,y,z):
     Return(X+Y+Z)
 def subtract(x,y):
     pass
-=======
-def add(x,y):
-    pass
-def subtract(x,y,z):
-    return(x-y-z):
->>>>>>> f2
 def multiply(x,y):
     pass
 def divide(x,y):
